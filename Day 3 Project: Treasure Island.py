b33320490e318fb019763438fd12c0efabb55a15
Alex Nyaga <75978088+NyagaA@users.noreply.github.com>
2024-01-14 00:57:58 +0300

    Create Day 3 Project: Treasure Island.py

---
+++ Day 3 Project: Treasure Island.py
@@ -0,0 +1,50 @@
+# TREASURE ISLAND
+print('''
+  ____________________________________________________________________
+ / \-----     ---------  -----------     -------------- ------    ----\
+ \_/__________________________________________________________________/
+ |~ ~~ ~~~ ~ ~ ~~~ ~ _____.----------._ ~~~  ~~~~ ~~   ~~  ~~~~~ ~~~~|
+ |  _   ~~ ~~ __,---'_       "         `. ~~~ _,--.  ~~~~ __,---.  ~~|
+ | | \___ ~~ /      ( )   "          "   `-.,' (') \~~ ~ (  / _\ \~~ |
+ |  \    \__/_   __(( _)_      (    "   "     (_\_) \___~ `-.___,'  ~|
+ |~~ \     (  )_(__)_|( ))  "   ))          "   |    "  \ ~~ ~~~ _ ~~|
+ |  ~ \__ (( _( (  ))  ) _)    ((     \\//    " |   "    \_____,' | ~|
+ |~~ ~   \  ( ))(_)(_)_)|  "    ))    //\\ " __,---._  "  "   "  /~~~|
+ |    ~~~ |(_ _)| | |   |   "  (   "      ,-'~~~ ~~~ `-.   ___  /~ ~ |
+ | ~~     |  |  |   |   _,--- ,--. _  "  (~~  ~~~~  ~~~ ) /___\ \~~ ~|
+ |  ~ ~~ /   |      _,----._,'`--'\.`-._  `._~~_~__~_,-'  |H__|  \ ~~|
+ |~~    / "     _,-' / `\ ,' / _'  \`.---.._          __        " \~ |
+ | ~~~ / /   .-' , / ' _,'_  -  _ '- _`._ `.`-._    _/- `--.   " " \~|
+ |  ~ / / _-- `---,~.-' __   --  _,---.  `-._   _,-'- / ` \ \_   " |~|
+ | ~ | | -- _    /~/  `-_- _  _,' '  \ \_`-._,-'  / --   \  - \_   / |
+ |~~ | \ -      /~~| "     ,-'_ /-  `_ ._`._`-...._____...._,--'  /~~|
+ | ~~\  \_ /   /~~/    ___  `---  ---  - - ' ,--.     ___        |~ ~|
+ |~   \      ,'~~|  " (o o)   "         " " |~~~ \_,-' ~ `.     ,'~~ |
+ | ~~ ~|__,-'~~~~~\    \"/      "  "   "    /~ ~~   O ~ ~~`-.__/~ ~~~|
+ |~~~ ~~~  ~~~~~~~~`.______________________/ ~~~    |   ~~~ ~~ ~ ~~~~|
+ |____~jrei~__~_______~~_~____~~_____~~___~_~~___~\_|_/ ~_____~___~__|
+ / \----- ----- ------------  ------- ----- -------  --------  -------\
+ \_/__________________________________________________________________/
+ ''')
+
+print("Welcome to Treasure Island!")
+print("Your mission is to find the treasure.")
+
+direction = input('You\'re at a crossroad, where do you want to go? Type "Left" or "Right".\n').upper()
+
+if direction == "LEFT":
+    lake = input("You have come to a lake. There is an island in the middle of the lake. Type 'Wait' to wait for a boat. Type 'Swim' to swim across.\n").upper()
+    if lake == "WAIT":
+        door = input("You have arrived at the island unharmed. There is a house with three doors. One red, one yellow and the other blue. Which colour would you choose?\n").upper()
+        if door == "BLUE":
+            print("Attacke by komodo dragons!")
+        elif door == "RED":
+            print("You found the treasure! You win!")
+        elif door == "YELLOW":
+            print("Headshot!")
+        else:
+            print("You broke your limbs!")
+    else:
+        print("You were attacked by crocodiles!\Game Over!")
+else:
+    print("You fell into a ditch of vipers!\nGame Over!")
